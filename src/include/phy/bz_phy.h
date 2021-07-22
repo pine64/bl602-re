0dc58f269a61eb0db02e4534a82df28638148956
Fangfei Yang <yangff1@gmail.com>
2021-07-22 01:10:49 -0500

    add support for 0x

--- src/include/phy/bz_phy.h
+++ src/include/phy/bz_phy.h
@@ -2,6 +2,40 @@ typedef union {
 	uint32_t regs[0x400];
 	uint8_t pad[0x1000];
 	struct {
+		uint8_t pad0[0x808];
+		union {
+			uint32_t value;
+			struct {
+				uint32_t pad0 : 23;
+				uint32_t set_0x1_67 : 1; // @ 23 -- 23 # 0xff7fffff
+				uint32_t set_0x8_68 : 8; // @ 31 -- 24 # 0xffffff
+			};
+		} r0x2808; // @ 0x808
+		union {
+			uint32_t value;
+			struct {
+				uint32_t set_0x0_71 : 8; // @ 7 -- 0 # 0xffffff00
+				uint32_t pad0 : 15;
+				uint32_t set_0x1_69 : 1; // @ 23 -- 23 # 0xff7fffff
+				uint32_t set_0x4_70 : 8; // @ 31 -- 24 # 0xffffff
+			};
+		} r0x280c; // @ 0x80c
+		union {
+			uint32_t value;
+			struct {
+				uint32_t set_0x0_76 : 1; // @ 0 -- 0 # 0xfffffffe
+			};
+		} r0x2810; // @ 0x810
+		uint8_t pad1[0x40];
+		union {
+			uint32_t value;
+			struct {
+				uint32_t set_0x1e_75 : 8; // @ 7 -- 0 # 0xffffff00
+				uint32_t set_0x20_72 : 8; // @ 15 -- 8 # 0xffff00ff
+				uint32_t set_0xa_74 : 8; // @ 23 -- 16 # 0xff00ffff
+				uint32_t set_0x1e_73 : 8; // @ 31 -- 24 # 0xffffff
+			};
+		} r0x2854; // @ 0x854
 	};
 } bz_phy_regs;
 #define BZ_PHY_BASE 0x40002000
