95cf6ec12e6426127f2bbbeed3b41916907e153a
Fangfei Yang <yangff1@gmail.com>
2021-07-24 02:22:45 -0500

    add extra names

--- src/include/phy/bz_phy.h
+++ src/include/phy/bz_phy.h
@@ -7,41 +7,41 @@ typedef union {
 			uint32_t value;
 			struct {
 				uint32_t pad0 : 23;
-				uint32_t set_0x1_67 : 1; // @ 23 -- 23 # 0xff7fffff
-				uint32_t set_0x8_68 : 8; // @ 31 -- 24 # 0xffffff
+				uint32_t bz_phy_tx_rampup_fm_on : 1; // @ 23 -- 23 # 0xff7fffff
+				uint32_t bz_phy_tx_rampup_time_us : 8; // @ 31 -- 24 # 0xffffff
 			};
 		} r0x2808; // @ 0x808
 		union {
 			uint32_t value;
 			struct {
-				uint32_t set_0x0_71 : 8; // @ 7 -- 0 # 0xffffff00
+				uint32_t bz_phy_tx_rampdn_pad0_time_us : 8; // @ 7 -- 0 # 0xffffff00
 				uint32_t pad0 : 15;
-				uint32_t set_0x1_69 : 1; // @ 23 -- 23 # 0xff7fffff
-				uint32_t set_0x4_70 : 8; // @ 31 -- 24 # 0xffffff
+				uint32_t bz_phy_tx_rampdn_fm_on : 1; // @ 23 -- 23 # 0xff7fffff
+				uint32_t bz_phy_tx_rampdn_time_us : 8; // @ 31 -- 24 # 0xffffff
 			};
 		} r0x280c; // @ 0x80c
 		union {
 			uint32_t value;
 			struct {
-				uint32_t set_0x0_76 : 1; // @ 0 -- 0 # 0xfffffffe
-				uint32_t set_0x1_77 : 1; // @ 1 -- 1 # 0xfffffffd
+				uint32_t bz_phy_rx_dfe_notch_en : 1; // @ 0 -- 0 # 0xfffffffe
+				uint32_t bz_phy_rx_dfe_toc_en : 1; // @ 1 -- 1 # 0xfffffffd
 			};
 		} r0x2810; // @ 0x810
 		uint8_t pad1[0x40];
 		union {
 			uint32_t value;
 			struct {
-				uint32_t set_0x1e_75 : 8; // @ 7 -- 0 # 0xffffff00
-				uint32_t set_0x20_72 : 8; // @ 15 -- 8 # 0xffff00ff
-				uint32_t set_0xa_74 : 8; // @ 23 -- 16 # 0xff00ffff
-				uint32_t set_0x1e_73 : 8; // @ 31 -- 24 # 0xffffff
+				uint32_t bz_phy_rx_proc_time_viterbi_us : 8; // @ 7 -- 0 # 0xffffff00
+				uint32_t bz_phy_rx_proc_time_mlsd_us : 8; // @ 15 -- 8 # 0xffff00ff
+				uint32_t bz_phy_rx_proc_time_eq_us : 8; // @ 23 -- 16 # 0xff00ffff
+				uint32_t bz_phy_rx_proc_time_direct_us : 8; // @ 31 -- 24 # 0xffffff
 			};
 		} r0x2854; // @ 0x854
 		uint8_t pad2[0x454];
 		union {
 			uint32_t value;
 			struct {
-				uint32_t set_0x4_78 : 5; // @ 4 -- 0 # 0xffffffe0
+				uint32_t bz_agc_rbb_ind_min : 5; // @ 4 -- 0 # 0xffffffe0
 			};
 		} r0x2cac; // @ 0xcac
 	};
