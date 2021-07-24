5ab3eca41430d5e3898c6370b9ef78cf8da08789
Fangfei Yang <yangff1@gmail.com>
2021-07-24 07:19:01 -0500

    fix var names

--- src/bl602_wifi/phy_bl602.c
+++ src/bl602_wifi/phy_bl602.c
@@ -58,84 +58,86 @@ void phy_get_version(uint32_t *version_1,uint32_t *version_2) {
 }
 
 void agc_config(void) {
-    AGC->RWNXAGCCNTL.set_0x0_0 = 0x0;
-    AGC->RWNXAGCCNTL.set_0x0_1 = 0x0;
-    AGC->r0xb3a4.set_0x0_2 = 0x0;
-    AGC->r0xb3a4.set_0x0_3 = 0x0;
-    AGC->r0xb394.set_0xf8_4 = 0xf8;
-    AGC->r0xb398.set_0x9e_5 = 0x9e;
-    AGC->r0xb3c4.set_0xce_6 = 0xce;
-    AGC->r0xb364.set_0x8_7 = 0x8;
-    AGC->r0xb364.set_0x3c_8 = 0x3c;
-    AGC->r0xb364.set_0x38_9 = 0x38;
-    AGC->r0xb364.set_0x39_10 = 0x39;
-    AGC->r0xb368.set_0x70_11 = 0x70;
-    AGC->r0xb368.set_0x70_12 = 0x70;
-    AGC->r0xb36c.set_0x12_13 = 0x12;
-    AGC->r0xb36c.set_0x5_14 = 0x5;
-    AGC->r0xb36c.set_0x28_15 = 0x28;
-    AGC->r0xb36c.set_0x7_16 = 0x7;
-    AGC->r0xb370.set_0x58_17 = 0x58;
-    AGC->r0xb3c0.set_0x18_18 = 0x18;
-    AGC->r0xb380.set_0x3e_19 = 0x3e;
-    AGC->r0xb380.set_0x37_20 = 0x37;
-    AGC->r0xb380.set_0x1_21 = 0x1;
-    AGC->r0xb380.set_0x0_22 = 0x0;
-    AGC->r0xb380.set_0x1_23 = 0x1;
-    AGC->r0xb384.set_0x39_24 = 0x39;
-    AGC->r0xb384.set_0x37_25 = 0x37;
-    AGC->r0xb384.set_0x14_26 = 0x14;
-    AGC->r0xb384.set_0x0_27 = 0x0;
-    AGC->r0xb384.set_0x2_28 = 0x2;
-    AGC->r0xb388.set_0xf_29 = 0xf;
-    AGC->r0xb388.set_0x17_30 = 0x17;
-    AGC->r0xb388.set_0x2a_31 = 0x2a;
-    AGC->r0xb388.set_0x0_32 = 0x0;
-    AGC->r0xb388.set_0x5_33 = 0x5;
-    AGC->r0xb38c.set_0x19_34 = 0x19;
-    AGC->r0xb38c.set_0x0_35 = 0x0;
-    AGC->r0xb38c.set_0xe_36 = 0xe;
-    AGC->r0xb38c.set_0x2_37 = 0x2;
-    AGC->r0xc830.set_0x3f_38 = 0x3f;
-    AGC->r0xc830.set_0x1_39 = 0x1;
-    AGC->r0xc830.set_0x36_40 = 0x36;
-    AGC->r0xc830.set_0x5_41 = 0x5;
-    AGC->r0xc814.set_0x0_42 = 0x0;
-    AGC->r0xc040.set_0xc_43 = 0xc;
-    AGC->r0xc040.set_0x3_44 = 0x3;
-    AGC->r0xc044.set_0x8_45 = 0x8;
-    AGC->r0xc044.set_0x0_46 = 0x0;
+    AGC->RWNXAGCCNTL.riu_htstfgainen = 0x0;
+    AGC->RWNXAGCCNTL.riu_rifsdeten = 0x0;
+    AGC->r0xb3a4.riu_fe20gain = 0x0;
+    AGC->r0xb3a4.riu_fe40gain = 0x0;
+    AGC->r0xb394.riu_vpeakadcqdbv = 0xf8;
+    AGC->r0xb398.riu_adcpowmindbm = 0x9e;
+    AGC->r0xb3c4.riu_adcpowsupthrdbm = 0xce;
+    AGC->r0xb364.riu_satdelay50ns = 0x8;
+    AGC->r0xb364.riu_sathighthrdbv = 0x3c;
+    AGC->r0xb364.riu_satlowthrdbv = 0x38;
+    AGC->r0xb364.riu_satthrdbv = 0x39;
+    AGC->r0xb368.riu_crossdnthrqdbm = 0x70;
+    AGC->r0xb368.riu_crossupthrqdbm = 0x70;
+    AGC->RWNXAGCRAMP.riu_rampupgapqdb = 0x12;
+    AGC->RWNXAGCRAMP.riu_rampupndlindex = 0x5;
+    AGC->RWNXAGCRAMP.riu_rampdngapqdb = 0x28;
+    AGC->RWNXAGCRAMP.riu_rampdnndlindex = 0x7;
+    AGC->r0xb370.riu_adcpowdisthrdbv = 0x58;
+    AGC->r0xb3c0.riu_idinbdpowgapdnqdbm = 0x18;
+    AGC->r0xb380.riu_evt0op3 = 0x3e;
+    AGC->r0xb380.riu_evt0op2 = 0x37;
+    AGC->r0xb380.riu_evt0op1 = 0x1;
+    AGC->r0xb380.riu_evt0pathcomb = 0x0;
+    AGC->r0xb380.riu_evt0opcomb = 0x1;
+    AGC->r0xb384.riu_evt1op1 = 0x39;
+    AGC->r0xb384.riu_evt1op2 = 0x37;
+    AGC->r0xb384.riu_evt1op3 = 0x14;
+    AGC->r0xb384.riu_evt1pathcomb = 0x0;
+    AGC->r0xb384.riu_evt1opcomb = 0x2;
+    AGC->r0xb388.riu_evt2op1 = 0xf;
+    AGC->r0xb388.riu_evt2op2 = 0x17;
+    AGC->r0xb388.riu_evt2op3 = 0x2a;
+    AGC->r0xb388.riu_evt2pathcomb = 0x0;
+    AGC->r0xb388.riu_evt2opcomb = 0x5;
+    AGC->r0xb38c.riu_evt3op1 = 0x19;
+    AGC->r0xb38c.riu_evt3op2 = 0x0;
+    AGC->r0xb38c.riu_evt3op3 = 0xe;
+    AGC->r0xb38c.riu_evt3opcomb = 0x2;
+    AGC->r0xc830.rc2_evt4op1 = 0x3f;
+    AGC->r0xc830.rc2_evt4op2 = 0x1;
+    AGC->r0xc830.rc2_evt4op3 = 0x36;
+    AGC->r0xc830.rc2_evt4opcomb = 0x5;
+    AGC->r0xc814.rc2_pkdet_mode = 0x0;
+    AGC->r0xc814.rc2_pkdet_cnt_thr = 0x2;
+    AGC->r0xc814.rc2_pkdet_cnt_thr = 0x2;
+    AGC->r0xc040.rc2_rx0_vga_idx_max = 0xc;
+    AGC->r0xc040.rc2_rx0_vga_idx_min = 0x3;
+    AGC->r0xc044.rc2_rx0_lna_idx_max = 0x8;
+    AGC->r0xc044.rc2_rx0_lna_idx_min = 0x0;
 
     phy_config_rxgain(0);
 
-    AGC->r0xb3a0.set_0x9e_47 = 0x9e;
-    AGC->r0xb3c0.set_0xa4_48 = 0xa4;
-    AGC->r0xb3c0.set_0xa3_49 = 0xa3;
-    AGC->r0xc82c.set_0xb5_50 = 0xb5;
-    AGC->r0xc82c.set_0x1_51 = 0x1;
-    AGC->r0xc82c.set_0x1_52 = 0x1;
-    AGC->r0xc838.set_0x1_53 = 0x1;
-    AGC->r0xc838.set_0x100_54 = 0x100;
-    AGC->r0xc83c.set_0x1_55 = 0x1;
-    AGC->r0xc83c.set_0x17c_56 = 0x17c;
-    AGC->r0xc840.set_0x1_57 = 0x1;
-    AGC->r0xc840.set_0x100_58 = 0x100;
-    AGC->r0xc82c.set_0x40_59 = 0x40;
+    AGC->r0xb3a0.riu_inbdpowmindbm = 0x9e;
+    AGC->r0xb3c0.riu_inbdpowsupthrdbm = 0xa4;
+    AGC->r0xb3c0.riu_inbdpowinfthrdbm = 0xa3;
+    AGC->r0xc82c.rc2_inbdpow_adj_thr_dbm = 0xb5;
+    AGC->r0xc82c.rc2_inbdpowsupthr_adj_en = 0x1;
+    AGC->r0xc82c.rc2_inbdpowinfthr_adj_en = 0x1;
+    AGC->r0xc838.rc2_reflevofdmthd_en = 0x1;
+    AGC->r0xc838.rc2_reflevofdmthd = 0x100;
+    AGC->r0xc83c.rc2_reflevdsssthd_en = 0x1;
+    AGC->r0xc83c.rc2_reflevdsssthd = 0x17c;
+    AGC->r0xc840.rc2_reflevdssscontthd_en = 0x1;
+    AGC->r0xc840.rc2_reflevdssscontthd = 0x100;
+    AGC->r0xc82c.rc2_inbdpowfastvalid_cnt = 0x40;
 }
 
 void bz_phy_reset(void) {
-    BZ_PHY->r0x2808.set_0x1_67 = 0x1;
-    BZ_PHY->r0x2808.set_0x8_68 = 0x8;
-    BZ_PHY->r0x280c.set_0x1_69 = 0x1;
-    BZ_PHY->r0x280c.set_0x4_70 = 0x4;
-    BZ_PHY->r0x280c.set_0x0_71 = 0x0;
-    BZ_PHY->r0x2854.set_0x20_72 = 0x20;
-    BZ_PHY->r0x2854.set_0x1e_73 = 0x1e;
-    BZ_PHY->r0x2854.set_0xa_74 = 0xa;
-    BZ_PHY->r0x2854.set_0x1e_75 = 0x1e;
-    BZ_PHY->r0x2810.set_0x0_76 = 0x0;
-    BZ_PHY->r0x2810.set_0x1_77 = 0x1;
-    BZ_PHY->r0x2cac.set_0x4_78 = 0x4;
+    BZ_PHY->r0x2808.bz_phy_tx_rampup_fm_on = 0x1;
+    BZ_PHY->r0x2808.bz_phy_tx_rampup_time_us = 0x8;
+    BZ_PHY->r0x280c.bz_phy_tx_rampdn_fm_on = 0x1;
+    BZ_PHY->r0x280c.bz_phy_tx_rampdn_time_us = 0x4;
+    BZ_PHY->r0x280c.bz_phy_tx_rampdn_pad0_time_us = 0x0;
+    BZ_PHY->r0x2854.bz_phy_rx_proc_time_mlsd_us = 0x20;
+    BZ_PHY->r0x2854.bz_phy_rx_proc_time_direct_us = 0x1e;
+    BZ_PHY->r0x2854.bz_phy_rx_proc_time_eq_us = 0xa;
+    BZ_PHY->r0x2854.bz_phy_rx_proc_time_viterbi_us = 0x1e;
+    BZ_PHY->r0x2810.bz_phy_rx_dfe_notch_en = 0x0;
+    BZ_PHY->r0x2810.bz_phy_rx_dfe_toc_en = 0x1;
+    BZ_PHY->r0x2cac.bz_agc_rbb_ind_min = 0x4;
     return ;
 }
 
@@ -193,17 +195,17 @@ int phy_freq_to_channel(uint8_t band, uint16_t freq);
 void phy_hw_set_channel(uint8_t band, uint16_t freq, uint16_t freq1, uint8_t chantype) {
     ASSERT_ERR(chantype == PHY_CHNL_BW_20);
     ASSERT_ERR(band == PHY_BAND_2G4);
-    AGC->RWNXAGCCNTL.set_0x0_61 = 0;
-    MDM->rxchan.reset = 1;
+    AGC->RWNXAGCCNTL.riu_ofdmonly = 0;
+    MDM->rxchan.rxdsssen = 1;
     MDM->mdmconf = 0;
     mdm_reset();
-    MDM->TXCTRL0 = 0xb4;
-    MDM->TXCTRL1 = 0x1c13;
-    MDM->TXCTRL3 = 0x2d00438;
+    MDM->txstartdelay = 0xb4;
+    MDM->txctrl1 = 0x1c13;
+    MDM->txctrl3 = 0x2d00438;
     MDM->TBECTRL0.tbe_count_adjust_20 = 0;
     MDM->DCESTIMCTRL.value = 0xf0f;
     MDM->DCESTIMCTRL.WAITHTSTF = 7;
-    MDM->r834.set60h = 0x6;
+    MDM->r834.tddchtstfmargin = 0x6;
     MDM->SMOOTHCTRL.value = 0x1880c06;
     MDM->tbectrl2 = 0x7f03;
     AGC->riu_rwnxagcaci20marg0 = 0;
@@ -256,51 +258,51 @@ void phy_init(phy_cfg_tag *config) {
     mdm_reset();
     MDM->rxchan.value = 0x20d;
     MDM->rxchan.rxnssmax = phy_get_nss();
-    MDM->rxchan.rxndpnstsmax = MDM->version.rxndpnstsmax;
-    MDM->rxchan.v19 = MDM->version.v19;
-    MDM->rxchan.vht = phy_vht_supported();
-    MDM->rxchan.v30 = MDM->version.mu_mimo_rx;
+    MDM->rxchan.rxndpnstsmax = MDM->version.nsts;
+    MDM->rxchan.rxldpcen = MDM->version.ldpcdec;
+    MDM->rxchan.rxvhten = phy_vht_supported();
+    MDM->rxchan.rxmumimoen = MDM->version.mu_mimo_rx;
     MDM->r3024.precomp = 0x2D;
     MDM->rxframeviolationmask = 0xffffffff;
 
     MDM->txchan.value = 0x20d;
     MDM->txchan.txnssmax = phy_get_nss();
     MDM->txchan.ntxmax = phy_get_ntx();
-    MDM->txchan.txcbwmax = MDM->version.txcbwmax;
-    MDM->txchan.v18 = MDM->version.v18;
+    MDM->txchan.txcbwmax = MDM->version.chbw;
+    MDM->txchan.txldpcen = MDM->version.ldpcenc;
     MDM->txchan.vht = phy_vht_supported();
-    MDM->txchan.v31 = MDM->version.mu_mimo_tx;
+    MDM->txchan.txmumimoen = MDM->version.mu_mimo_tx;
     
-    MDM->r834.set1 = 1;
+    MDM->r834.rxtdctrl1 = 1;
     MDM->SMOOTHCTRL.CFGSMOOTHFORCE = 0;
     MDM->SMOOTHSNRTHR.smoothsnrthrhigh = 0x1b;
     MDM->SMOOTHSNRTHR.smoothsnrthrmid = 0xf;
     MDM->rxctrl1 = 0x4920492;
-    MDM->r874.resetto1 = 1;
+    MDM->r0x874.rcclkforce = 1;
     
-    AGC->r0xb500.set_0x2_62 = 0x02;
+    AGC->r0xb500.riu_txshift4044 = 0x02;
     if (AGC->r000.iqcomp) {
-        AGC->r0xb110.set_0x0_63 = 0;
-        AGC->r0xb110.set_0x0_64 = 0;
-        AGC->r0xb110.set_0x0_65 = 0;
-        AGC->r0xb110.set_0x0_66 = 0;
+        AGC->r0xb110.riu_rxiqphaseesten = 0;
+        AGC->r0xb110.riu_rxiqgainesten = 0;
+        AGC->r0xb110.riu_rxiqphasecompen = 0;
+        AGC->r0xb110.riu_rxiqgaincompen = 0;
         AGC->riu_iqestiterclr = 0;
     }
     AGC->activeant = 1;
-    AGC->RWNXAGCCNTL.set1 = 1;
+    AGC->RWNXAGCCNTL.combpathsel = 1;
     AGC->RWNXAGCCCATIMEOUT = 4000000;
-    AGC->irqmacccatimeouten.set1 = 1;
+    AGC->irqmacccatimeouten.irqmacccatimeouten = 1;
     agc_config();
     AGC->RWNXAGCCNTL.agcfsmreset = 1;
 
-    MDM->r874.set1beforewriteagcmem = 1;
+    MDM->r0x874.mdm_agcmemclkforce = 1;
 
     for (int i = 0; i < 0x200; i++)
         AGCRAM->agcram[i] = agcmem[i];
     
-    MDM->r874.set1beforewriteagcmem = 0;
+    MDM->r0x874.mdm_agcmemclkforce = 0;
     AGC->RWNXAGCCNTL.agcfsmreset = 0;
-    AGC->r0xc020.set_0x14_60 = 0x14;
+    AGC->r0xc020.rc_paoff_delay = 0x14;
     phy_env[0].cfg.reserved = (config->parameters)[0];
     phy_env[0].chnl_center1_freq = 0xff;
     phy_env[0].chnl_center2_freq = 0xff;
@@ -316,7 +318,7 @@ void phy_init(phy_cfg_tag *config) {
 uint8_t phy_vht_supported(){
     if (MDM->version.vht) // TODO: verify this cond
         return 1;
-    return ((MDM->version.txcbwmax) >> 1) & 1;
+    return ((MDM->version.chbw) >> 1) & 1;
 }
 
 uint8_t phy_ldpc_rx_supported(void){
@@ -340,8 +342,8 @@ uint8_t phy_mu_mimo_tx_supported(void) {
 }
 
 void phy_rc_isr(void) {
-    uint32_t val = AGC->r41c.value;
-    AGC->r420_copy41c = val;
+    uint32_t val = AGC->rwnxmacintstatmasked.value;
+    AGC->rwnxmacintack = val;
     if (val & 0x100) {
         mdm_reset();
     }
