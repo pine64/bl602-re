f4959fc0ae1a4060c9ad6d5e78d41772818a62de
Fangfei Yang <yangff1@gmail.com>
2021-07-25 02:29:37 -0500

    add memcpy and min/max

--- src/bl602_wifi/utils.h
+++ src/bl602_wifi/utils.h
@@ -1,5 +1,9 @@
 #ifndef __UTILS_H_
 #define ABS(x) (((x) > 0) ? (x) : (-(x)))
+#define MIN(x,y) (((x) > (y)) ? (y) : (x))
+#define MAX(x,y) (((x) < (y)) ? (y) : (x))
 int puts(char *s);
 int printf(char *fmt,...);
+void* memset(void *, int,  long unsigned int);
+void* memcpy(void*, const void*, unsigned int);
 #endif
