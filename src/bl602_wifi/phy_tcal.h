1106214d88b99551dad0b61c4362653ba9e03fb4
Fangfei Yang <yangff1@gmail.com>
2021-07-25 02:20:48 -0500

    add tcal

--- src/bl602_wifi/phy_tcal.h
+++ src/bl602_wifi/phy_tcal.h
@@ -1,6 +1,21 @@
 #ifndef _PHY_TCAL_H_
 #define _PHY_TCAL_H_
+
+#include <stdint.h>
+
 void phy_tcal_reset(void);
 void phy_tcal_start(void);
 
+struct tcal_tag {
+    int16_t prev_temperature; // +0
+    uint32_t last_action_time[4]; // +4
+    uint32_t last_action_temperature[4]; // +20
+    int32_t last_action_out[4]; // +36
+    bool enabled; // +52
+}; // :29:8
+
+void phy_tcal_stop(void); // :69:6
+void phy_tcal_handle(void); // :74:6
+void phy_tcal_callback(int16_t temperature); // :104:6
+void phy_tcal_txpwr(int16_t curr_temperature); // :141:6
 #endif
