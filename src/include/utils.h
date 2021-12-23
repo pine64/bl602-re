6537668c32049edf173a218e742b43f7d4e6b37b
Fangfei Yang <yangff1@gmail.com>
2021-12-23 05:21:28 -0600

    reduce warning

--- src/include/utils.h
+++ src/include/utils.h
@@ -3,8 +3,8 @@
 #define MIN(x,y) (((x) > (y)) ? (y) : (x))
 #define MAX(x,y) (((x) < (y)) ? (y) : (x))
 int puts(char *s);
-int printf(char *fmt,...);
-void* memset(void *, int,  long unsigned int);
+int printf(const char *fmt,...);
+void* memset(void *, int, unsigned int);
 void* memcpy(void*, const void*, unsigned int);
 void BL602_Delay_US(uint32_t cnt);
 void BL602_Delay_MS(uint32_t cnt);
