7b1f08eb153f567f1225a2c0454cc0a1d90b0ef7
Fangfei Yang <yangff1@gmail.com>
2021-12-24 04:07:55 -0600

    add header

--- src/bl602_wifi/driver/sysctrl/sysctrl.c
+++ src/bl602_wifi/driver/sysctrl/sysctrl.c
@@ -1,5 +1,7 @@
 #include <stdint.h>
 #include <phy/sysctrl.h>
+#include <sysctrl/sysctrl.h>
+
 void sysctrl_init() {
     SYSCTRL->diag_conf.value = 0x8000000c;
     SYSCTRL->misc_cntl.set1 = 0x1ff;
