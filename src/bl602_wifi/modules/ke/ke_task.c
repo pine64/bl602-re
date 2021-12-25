5b5be2c1eda3c6d70f8eade5ef907721b1c0ea38
Fangfei Yang <yangff1@gmail.com>
2021-12-25 06:27:19 -0600

    Finish ke_task

--- src/bl602_wifi/modules/ke/ke_task.c
+++ src/bl602_wifi/modules/ke/ke_task.c
@@ -1,5 +1,8 @@
 #include <modules/ke/ke_task.h>
 #include <modules/ke/ke_msg.h>
+#include <modules/ke/ke_env.h>
+#include <modules/ke/ke_event.h>
+#include <modules/ke/ke_queue.h>
 
 #include <lmac/mm/mm_task.h>
 #include <modules/dbg/dbg_task.h>
@@ -12,14 +15,16 @@
 #include <umac/hostapd/hostapd_task.h>
 #include <cfg/cfg_task.h>
 
+#include <arch/rv32i/ll.h>
+
 #include <stddef.h>
 
 static const struct ke_task_desc TASK_DESC[TASK_MAX] = {
 	/// MAC Management task.
 	[TASK_MM] = {
-		.state_handler = &mm_state_handler, 
+		.state_handler = mm_state_handler, 
 		.default_handler = &mm_default_handler,
-		.state = &mm_state, 
+		.state = mm_state, 
 		.state_max = MM_STATE_MAX,
 		.idx_max = MM_IDX_MAX,
 	},
@@ -34,7 +39,7 @@ static const struct ke_task_desc TASK_DESC[TASK_MAX] = {
 	[TASK_SCAN] = {
 		.state_handler = NULL, 
 		.default_handler = &scan_default_handler,
-		.state = &scan_state, 
+		.state = scan_state, 
 		.state_max = SCAN_STATE_MAX,
 		.idx_max = SCAN_ID_MAX,
 	},
@@ -42,9 +47,9 @@ static const struct ke_task_desc TASK_DESC[TASK_MAX] = {
 	[TASK_TDLS] = {0},
 	/// SCAN task
 	[TASK_SCANU] = {
-		.state_handler = &scanu_state_handler, 
+		.state_handler = scanu_state_handler, 
 		.default_handler = &scanu_default_handler,
-		.state = &scanu_state, 
+		.state = scanu_state, 
 		.state_max = SCANU_STATE_MAX,
 		.idx_max = SCANU_IDX_MAX,
 	},
@@ -52,7 +57,7 @@ static const struct ke_task_desc TASK_DESC[TASK_MAX] = {
 	[TASK_ME] = {
 		.state_handler = NULL, 
 		.default_handler = &me_default_handler,
-		.state = &me_state, 
+		.state = me_state, 
 		.state_max = ME_STATE_MAX,
 		.idx_max = ME_IDX_MAX,
 	},
@@ -60,7 +65,7 @@ static const struct ke_task_desc TASK_DESC[TASK_MAX] = {
 	[TASK_SM] = {
 		.state_handler = NULL, 
 		.default_handler = &sm_default_handler,
-		.state = &sm_state,
+		.state = sm_state,
 		.state_max = SM_STATE_MAX,
 		.idx_max = SM_IDX_MAX,
 	},
@@ -68,7 +73,7 @@ static const struct ke_task_desc TASK_DESC[TASK_MAX] = {
 	[TASK_APM] = {
 		.state_handler = NULL, 
 		.default_handler = &apm_default_handler,
-		.state = &apm_state, 
+		.state = apm_state, 
 		.state_max = APM_STATE_MAX,
 		.idx_max = APM_IDX_MAX,
 	},
@@ -76,7 +81,7 @@ static const struct ke_task_desc TASK_DESC[TASK_MAX] = {
 	[TASK_BAM] = {
 		.state_handler = NULL, 
 		.default_handler = &bam_default_handler,
-		.state = &bam_state,
+		.state = bam_state,
 		.state_max = BAM_STATE_MAX,
 		.idx_max = BAM_IDX_MAX,
 	},
@@ -85,7 +90,7 @@ static const struct ke_task_desc TASK_DESC[TASK_MAX] = {
 	[TASK_HOSTAPD] = {
 		.state_handler = NULL, 
 		.default_handler = &hostapd_default_handler,
-		.state = &hostapd_u_state,
+		.state = hostapd_u_state,
 		.state_max = HOSTAPD_STATE_MAX,
 		.idx_max = HOSTAPD_IDX_MAX,
 	},
@@ -95,7 +100,7 @@ static const struct ke_task_desc TASK_DESC[TASK_MAX] = {
 	[TASK_CFG] = {
 		.state_handler = NULL, 
 		.default_handler = &cfg_default_handler,
-		.state = &cfg_state, 
+		.state = cfg_state, 
 		.state_max = CFG_STATE_MAX,
 		.idx_max = CFG_IDX_MAX,
 	},
@@ -109,38 +114,105 @@ static bool cmp_dest_id(const struct co_list_hdr *msg, uint32_t dest_id) {
 }
 
 static void ke_task_saved_update(const ke_task_id_t ke_task_id) {
-	__builtin_trap();
+	while (true) {
+		struct ke_msg* msg = (struct ke_msg*) ke_queue_extract(&ke_env.queue_saved, &cmp_dest_id, (uint32_t) ke_task_id);
+		if (msg) {
+			__disable_irq();
+			ke_queue_push(&ke_env.queue_sent, (struct co_list_hdr*)msg);
+			__enable_irq();
+			ke_evt_set(KE_EVT_KE_MESSAGE_BIT);
+		} else return ;
+	}
 }
 
 void ke_state_set(const ke_task_id_t id, const ke_state_t state_id) {
-	__builtin_trap();
+    int type = KE_TYPE_GET(id);
+    int idx = KE_IDX_GET(id);
+	ASSERT_ERR(type < TASK_MAX);
+    ASSERT_ERR(ke_task_local(type));
+    ASSERT_ERR(idx < TASK_DESC[type].idx_max);
+	ke_state_t *ke_stateid_ptr = TASK_DESC[type].state + idx;
+	ASSERT_ERR(ke_stateid_ptr);
+	if (*ke_stateid_ptr != state_id) {
+		*ke_stateid_ptr = state_id;
+		ke_task_saved_update(id);
+	}
 }
 
 ke_state_t ke_state_get(const ke_task_id_t id) {
-	__builtin_trap();
+    int type = KE_TYPE_GET(id);
+    int idx = KE_IDX_GET(id);
+	ASSERT_ERR(type < TASK_MAX);
+	ASSERT_ERR(ke_task_local(type));
+	ASSERT_ERR(idx < TASK_DESC[type].idx_max);
+	return TASK_DESC[idx].state[type];
 }
 
-static ke_msg_func_t ke_handler_search(const ke_msg_id_t msg_id, const struct ke_state_handler *state_handler)
-{
-	__builtin_trap();
+static ke_msg_func_t ke_handler_search(const ke_msg_id_t msg_id, const struct ke_state_handler *state_handler) {
+	if (state_handler->msg_cnt == 0)
+		return NULL;
+	for (int i = ((int)state_handler->msg_cnt) - 1; i >= 0; i--) {
+		if (state_handler->msg_table[i].id == msg_id) {
+			ASSERT_ERR(state_handler->msg_table[i].func);
+			return state_handler->msg_table[i].func;
+		}
+	}
+	return NULL;
 }
 
-ke_msg_func_t ke_task_handler_get(const ke_msg_id_t msg_id, const ke_task_id_t task_id)
-{
-	__builtin_trap();
+static ke_msg_func_t ke_task_handler_get(const ke_msg_id_t msg_id, const ke_task_id_t task_id) {
+    int type = KE_TYPE_GET(task_id);
+    int idx = KE_IDX_GET(task_id);
+
+	ASSERT_ERR(type < TASK_MAX);
+	ASSERT_ERR(ke_task_local(type));
+	ASSERT_ERR(idx < TASK_DESC[type].idx_max);
+
+	const struct ke_task_desc *desc = TASK_DESC + type;
+	ASSERT_ERR(desc);
+
+	const struct ke_state_handler * handler = desc->state_handler ? (desc->state_handler + desc->state[idx]) 
+																  : (desc->default_handler);
+	return ke_handler_search(msg_id, handler);;
 }
 
-void ke_task_schedule(int dummy)
-{
-	__builtin_trap();
+void ke_task_schedule(int dummy) {
+	do {
+		__disable_irq();
+		struct ke_msg * msg = (struct ke_msg *) ke_queue_pop(&ke_env.queue_sent);
+		__enable_irq();
+		if (!msg)
+			break;
+		ke_msg_func_t func = ke_task_handler_get(msg->id, msg->dest_id);
+		enum ke_msg_status_tag status = KE_MSG_CONSUMED;
+		if (func) {
+			status = (enum ke_msg_status_tag) func(msg->id, ke_msg2param(msg), msg->dest_id, msg->src_id);
+		}
+		switch (status) {
+		case KE_MSG_CONSUMED:
+			ke_msg_free(msg);
+		case KE_MSG_NO_FREE:
+			break;
+		case KE_MSG_SAVED:
+			ke_queue_push(&ke_env.queue_saved, (struct co_list_hdr*) msg);
+			break;
+		default:
+			ASSERT_ERR(0);
+		}
+
+	} while (0);
+
+	__disable_irq();
+	if (ke_env.queue_sent.first == 0x0) {
+		ke_evt_clear(KE_EVT_KE_MESSAGE_BIT);
+	}
+	__enable_irq();
 }
 
-int ke_msg_discard(const ke_msg_id_t msgid, const void *param, const ke_task_id_t dest_id, const ke_task_id_t src_id)
-{
-	__builtin_trap();
+int ke_msg_discard(const ke_msg_id_t msgid, const void *param, const ke_task_id_t dest_id, const ke_task_id_t src_id) {
+	return KE_MSG_CONSUMED;
 }
 
-int ke_msg_save(const ke_msg_id_t msgid, const void *param, const ke_task_id_t dest_id, const ke_task_id_t src_id)
-{
-	__builtin_trap();
+int ke_msg_save(const ke_msg_id_t msgid, const void *param, const ke_task_id_t dest_id, const ke_task_id_t src_id) {
+	return KE_MSG_SAVED;
 }
