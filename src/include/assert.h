3b6915c0fcd471161f3d5c4168d19aa107608029
Fangfei Yang <yangff1@gmail.com>
2021-12-28 03:45:08 -0600

    add ASSERT_WARN

--- src/include/assert.h
+++ src/include/assert.h
@@ -4,6 +4,7 @@
 #include <arch/arch.h>
 
 #define ASSERT_ERR(cond) if (!(cond)) assert_err(#cond, __FILE__, __LINE__);
+#define ASSERT_WARN(cond) if (!(cond)) assert_warn(#cond, __FILE__, __LINE__);
 
 #define ASSERT_REC_VAL(cond, ret) if (!(cond)) { assert_rec(#cond, __FILE__, __LINE__); return (ret); }
 #endif
