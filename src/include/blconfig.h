85d5ef690db665788028b9686e2f8df1a8e3996f
Fangfei Yang <yangff1@gmail.com>
2021-12-28 03:45:29 -0600

    moving config to seperated file

---
+++ src/include/blconfig.h
@@ -0,0 +1,16 @@
+#ifndef _BLCONFIG_H_
+#define _BLCONFIG_H_
+
+#ifndef  CFG_TXDESC
+#define CFG_TXDESC          4
+#endif
+#define NX_TXDESC_CNT0      CFG_TXDESC
+#define NX_CHAN_CTXT_CNT        3
+
+#define NX_REMOTE_STA_MAX 2
+
+#define NX_REMOTE_STA_MAX 2
+// == txl_buffer_control_desc_bcmc
+#define NX_VIRT_DEV_MAX 2
+
+#endif
