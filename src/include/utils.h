3faaafb821e865b807b7f65dfd6296b3f6298999
Fangfei Yang <yangff1@gmail.com>
2021-12-28 03:44:59 -0600

    add memcmp

--- src/include/utils.h
+++ src/include/utils.h
@@ -6,6 +6,7 @@ int puts(char *s);
 int printf(const char *fmt,...);
 void* memset(void *, int, unsigned int);
 void* memcpy(void*, const void*, unsigned int);
+int memcmp(const void *, const void *, unsigned int);
 void BL602_Delay_US(uint32_t cnt);
 void BL602_Delay_MS(uint32_t cnt);
 #endif
